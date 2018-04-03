59d314b29b5ecde319b8b4e47098840903d19732
Alexey Isotov <alexisotov@gmail.com>
2018-04-03 15:26:04 +0300

    Padding at navBar

--- src/components/NotFoundPage.js
+++ src/components/NotFoundPage.js
@@ -7,7 +7,7 @@ export class NotFoundPage extends Component {
  
     return (
 <div>
-		<h2 className=" text-center"> 404 Not found!</h2>
+		<h2 className=" text-center"> 404 Not found</h2>
 		<div className=" text-center">
 			<Link to="/">Go back</Link>
 		</div>
