1820e69e53cdea349f707e9bfdd4878f387c8dca
Alexey Isotov <alexisotov@gmail.com>
2018-04-03 14:23:12 +0300

    Validation check.AboutPage

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import {Footer} from './Footer.js';
 import {NavBar} from './navBar.js';
+import {InputAboutPage} from './InputAboutPage.js';
 
 export class AboutPage extends Component {
   constructor(props){
@@ -21,10 +22,8 @@ export class AboutPage extends Component {
 <div>
 		<h1 className="jumbotron display-1 text-center"> About page</h1>
 		<NavBar />
-		<p>Please enter your name</p>
-		<input type="text" value={this.state.name} onChange={this.onChange} />
-		<p> {this.state.name} is a good boy ! </p>
-		<h2 className="alert alert-success text-center"> Created by Richard the Dog aka Gangsta doog</h2>
+		<InputAboutPage />
+		<h2 className="alert alert-success text-center"> Created by Richard the Dog aka Gangsta dog</h2>
 		<Footer />
 </div>
 )
---
+++ src/components/InputAboutPage.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+
+export class InputAboutPage extends Component {
+  constructor(props){
+	  super(props);
+	  let name=props.name;
+	  let age=props.age;
+	  	  
+	  this.state={
+		  name: name,
+		  age: age,
+		  nameValid: null,
+		  ageValid: null,
+		  nameMessage:null,
+		  ageMessage:null
+	  };
+	  
+  }
+   
+  onAgeChange = e=> {
+	  let val = e.target.value;
+	  val<2? this.setState({age: val, ageValid:false,ageMessage:" You're to young to say hi!!!"}):val>110 ? this.setState({age: val, ageValid:false, ageMessage:" Are you zombie ??! "}):this.setState({age: val, ageValid:true, ageMessage:" Your age is OK!"});
+  
+  }
+  onNameChange = e=> {
+	  let val=e.target.value;
+	  val.length>2?this.setState({name:val, nameValid:true,nameMessage:"Name is OK !"}):this.setState({name:val, nameValid:false,nameMessage:"Too short name!"}); 
+  }
+  onclickInfo = e =>{
+	  e.preventDefault();
+	  (this.state.nameValid ===true && this.state.ageValid ===true) ? alert("Hello to you amigo  " + this.state.name + " " +this.state.age+ " years old"):alert("Check your inputs!");
+  }
+  render() {
+ let nameColor=this.state.nameValid===true?"green":"tomato";
+ let ageColor=this.state.nameValid===true?"green":"tomato";
+    return (
+<div className="formAbout">
+	<form  onSubmit={this.onclickInfo}>
+		<input type="text"placeholder="Enter your name" value={this.state.name} onChange={this.onNameChange} style={{borderColor:nameColor}}/>
+		<input type="number" placeholder="Enter your age" value={this.state.age} onChange={this.onAgeChange} style={{borderColor:ageColor}}/>
+		<input className="btn btn-primary" type="submit" value="say HI" />
+		<p className="alert alert-warning text-center">{this.state.nameMessage}  {this.state.ageMessage}</p>
+		
+	</form>
+		
+</div>
+)
+  }
+}
+export default InputAboutPage;
