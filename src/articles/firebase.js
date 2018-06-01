50ef0bdb0c460f1ac33c37a9bbcb21b7ad2043bb
AlexIsotov <alexisotov@gmail.com>
2018-06-01 22:18:11 +0300

    Update firebase.js

--- src/articles/firebase.js
+++ src/articles/firebase.js
@@ -1,12 +1,12 @@
 import firebase from 'firebase';
   // Initialize Firebase
  const config = {
-    apiKey: "AIzaSyDMM0VvXs0KJg0RRpK3NapJy0QH2x8WJLs",
-    authDomain: "r-blog-2740d.firebaseapp.com",
-    databaseURL: "https://r-blog-2740d.firebaseio.com",
-    projectId: "r-blog-2740d",
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
     storageBucket: "",
-    messagingSenderId: "107265170013"
+    messagingSenderId: ""
   };
   firebase.initializeApp(config);
 export default firebase;
